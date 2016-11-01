623815b0f898ca868750402bc2de024c03a61a61
AndresBarbosa <andresbob@ciencias.unam.mx>
2016-10-31 23:11:20 -0600

    Se ha añadido el hola mundo en c

---
+++ hola_mundo.c
@@ -0,0 +1,10 @@
+//incluye la biblioteca que permite imprimir en pantalla
+#include <stdio.h>
+
+//Metodo principal para mandar a llamar a la funcion print de stdio.h
+int main(){
+	//Se manda a llamar a la funcion print con el arreglo de caracteres "hola mundo"
+	printf("¡Hola, mundo!");
+	//Se regresa  por defecto, esto indica que no hubo ningun error durante la ejecucion
+	return 0;
+}
